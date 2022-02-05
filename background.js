6b480c18f9f3d87915d4b2a273fa82aad89a9d72
ShmatEugene <shmat.evg@yandex.ru>
2022-02-05 07:49:47 +0300

    parse xml

--- background.js
+++ background.js
@@ -1,6 +1,3 @@
-
-const API_URL = 'http://localhost:5000/api';
-
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.toDo === 'showPageAction') {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
@@ -8,42 +5,30 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     });
   }
 });
-//https://content.cambridgeone.org/cup1/products/evpel4/23/assets/ext-cup-xapiscoreable/1547448769184/data.js
 
-chrome.webRequest.onCompleted.addListener(
+chrome.webRequest.onBeforeRequest.addListener(
   function (details) {
     console.log(details);
-    return {
-      redirectUrl: 'https://content.cambridgeone.org/cup1/products/evpel4/23/assets/ext-cup-xapiscoreable/1547448769184/data.js'
-    };
+
+    if (details.tabId !== -1) {
+      fetch(details.url)
+        .then(function (response) {
+          return response.text();
+        })
+        .then(function (data) {
+          chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            chrome.tabs.sendMessage(
+              tabs[0].id,
+              {
+                toDo: 'sendAjaxData',
+                data: data,
+              },
+              function (response) {},
+            );
+          });
+        });
+    }
   },
   { urls: ['*://content.cambridgeone.org/*/data.js'] },
-  ['responseHeaders'],
+  ['extraHeaders'],
 );
-
-// chrome.webRequest.onBeforeRequest.addListener(
-//   function (details) {
-//     if (
-//       details.url.indexOf('://fpm.megaton-net.ru/Requests/IndexData') != -1 &&
-//       details.url.indexOf('isUpdatingRecords=1') === -1
-//     ) {
-//       console.log(details.url);
-//       //Если запрос на поиск по телу
-//       if (details.url.indexOf('sSearch=&') === -1) {
-//         return {
-//           redirectUrl: details.url.replace('sEcho=NaN', 'sEcho=1'),
-//         };
-//       } else {
-//         const urlSearchParams = new URLSearchParams(details.url);
-//         const params = Object.fromEntries(urlSearchParams.entries());
-//         console.log(params);
-//         return {
-//           redirectUrl: `${API_URL}/records/getFiltered?client=${params.sSearch_6}&sEcho=${params.sEcho}&iDisplayStart=${params.iDisplayStart}&iDisplayLength=${params.iDisplayLength}&Id=${params.sSearch_1}&Location=${params.sSearch_5}&Type=${params.sSearch_8}&SortBy=${params.iSortCol_0}&SortOrder=${params.sSortDir_0}`,
-//         };
-//       }
-//     }
-//     return;
-//   },
-//   { urls: ['*://fpm.megaton-net.ru/Requests/IndexData*'] },
-//   ['blocking'],
-// );
